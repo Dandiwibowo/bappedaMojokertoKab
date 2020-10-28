91b735db01974b854b26660c0e46b14c295bebee
NurrachmadWidyanto <didinrahmad@gmail.com>
2020-10-28 11:26:16 +0700

    penambahan comment modal add

--- connect.php
+++ connect.php
@@ -2,13 +2,13 @@
 $host ="localhost"; // Ubah sesuai hostname database
 $user ="root";  // Ubah sesuai username database
 $pass ="";  // Ubah sesuai password database
-$database ="kmkprobolinggo";  // Ubah sesuai nama database
+$database ="bappedadb";  // Ubah sesuai nama database
 $connect=mysqli_connect($host,$user,$pass,$database) or die ("gagal"); 
 
 if(!$connect)
 	echo "Can't Connected";
 
-$URL="http://localhost/kmkprobolinggokab";  // Ubah sesuai domain webiste
+$URL="http://localhost/Bapedda/bappedaMojokertoKab";  // Ubah sesuai domain webiste
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
 include "dbFunction.php";
